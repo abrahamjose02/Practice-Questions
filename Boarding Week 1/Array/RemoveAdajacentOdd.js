308276b5a9fe77f469ab4afcbbc7bc3b0392d887
Abraham Jose <abrahamjose002@gmail.com>
2024-11-10 17:37:09 +0530

    Remove Adjacent Odd elements

---
+++ Boarding Week 1/Array/RemoveAdajacentOdd.js
@@ -0,0 +1,11 @@
+function RemoveAdjacentOdd(arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] %2 !== 0 && arr[i+1] % 2 !== 0){
+            arr.splice(i,1)
+        }
+    }
+    return arr
+}
+
+const result = RemoveAdjacentOdd([1,2,3,5,6,7,9,10])
+console.log(result)
