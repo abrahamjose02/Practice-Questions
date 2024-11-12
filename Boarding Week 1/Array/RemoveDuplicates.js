e089d575f603edf173f74526308e81e42da84cdf
Abraham Jose <abrahamjose002@gmail.com>
2024-11-12 11:43:03 +0530

    RemoveDuplicatesFromArray

--- Boarding Week 1/Array/RemoveDuplicates.js
+++ Boarding Week 1/Array/RemoveDuplicates.js
@@ -38,3 +38,24 @@ function removeDuplicatesFromArray(arr){
   }
   
   
+  //another approach
+
+
+
+function RemoveDuplicate(arr){
+  for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[i]===arr[j]){
+        arr.splice(j,1)
+        j--
+      }
+    }
+  }
+  return arr
+}
+
+
+
+const arr1 = [1, 2, 3, 2, 4, 3, 5];
+
+console.log(RemoveDuplicate(arr1))
