3465ca637cb3f8484e92790f26a8d8ec8acc2458
Abraham Jose <abrahamjose002@gmail.com>
2024-11-12 12:02:05 +0530

    RemoveLastKeyOfObjectWithNestedValue

--- Boarding Week 1/Array/RemoveLastKeyOfObject.js
+++ Boarding Week 1/Array/RemoveLastKeyOfObject.js
@@ -9,3 +9,32 @@ function removeLastProperty(obj){
 
 let exampleObject = { name: "Joe", email: "Joe@gmail.com", age: 30 };
 console.log(removeLastProperty(exampleObject));
+
+
+
+function removeLastProperty(obj){
+    let keys = Object.keys(obj)
+    
+    if(keys.length === 0){
+      return obj
+    }
+    
+    let lastKey = keys[keys.length-1]
+    if(typeof obj[lastKey] === 'object' && obj[lastKey] !== null){
+      removeLastProperty(obj[lastKey])
+    }
+    delete obj[lastKey]
+    return obj
+  }
+  
+  
+  
+  let exampleObject2 = {
+      name: "Joe",
+      email: "Joe@gmail.com",
+      details: {
+          age: 30,
+          address: "123 Main St"
+      }
+  };
+  console.log(removeLastProperty(exampleObject2));
